b645452d138abc78e3c3936d85d2394c9d77b510
Jackson Geer <jacksongeer@gmail.com>
2023-09-21 15:13:07 -0400

    changed to custom counter app

--- bloc_practice/lib/main.dart
+++ bloc_practice/lib/main.dart
@@ -16,37 +16,81 @@ class BlocPractice extends StatefulWidget {
 }
 
 class _BlocPracticeState extends State<BlocPractice> {
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blueGrey,
-        title: Text('bloc practice app',
+        backgroundColor: Color.fromARGB(255, 59, 76, 85),
+        title: Text('Jackson\s Counter App',
             style: GoogleFonts.montserrat(
               fontWeight: FontWeight.bold,
             )),
         centerTitle: true,
       ),
-      body: Center(
-        child: Container(
-          height: 100,
-          width: 100,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: const Color.fromARGB(255, 255, 255, 255),
-            border: Border.all(width: 2.0, color: Colors.blueGrey),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.home),
-              Text(
-                'Blocs!',
-                style: GoogleFonts.montserrat(
-                  fontWeight: FontWeight.bold,
-                ),
+      persistentFooterButtons: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+          FloatingActionButton(onPressed: () {
+          setState(() {
+            counter--;
+            print(counter);
+          });
+        },
+        backgroundColor: Colors.blueGrey,
+        child: Icon(Icons.remove),),  
+        FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            counter++;
+            print(counter);
+          });
+        },
+        backgroundColor: Colors.blueGrey,
+        child: Icon(Icons.add),
+      ),],
+      ),
+      ],
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.blueGrey, Colors.white])),
+        child: Center(
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                counter = 0;
+                print(counter);
+              });
+            },
+            child: Container(
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: const Color.fromARGB(255, 255, 255, 255),
+                border: Border.all(
+                    width: 2.0, color: Color.fromARGB(255, 56, 73, 82)),
               ),
-            ],
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(counter.toString(), style: GoogleFonts.montserrat(fontWeight: FontWeight.bold, fontSize: 16),),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(6, 6, 6, 0),
+                    child: Text(
+                      'reset',
+                      style: GoogleFonts.montserrat(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
